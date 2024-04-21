f8d94d9f276e1d34ad9d50d17607e76b6ae0b7b6
Anatoly <etozhekoala@gmail.com>
2024-04-21 16:59:58 +0700

    feat: Вывести в консоль карточки

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 import './scss/styles.scss';
+import { API_URL } from './utils/constants';
+
+
+
+
+
+fetch(`${API_URL}/product`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  });
+
 
