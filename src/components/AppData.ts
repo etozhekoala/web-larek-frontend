0a29a45b55e8c886ef83fdb586b7ab7fa72ea7e2
Anatoly <etozhekoala@gmail.com>
2024-04-24 23:03:38 +0700

    feat: Добавить методы для класса

--- src/components/AppData.ts
+++ src/components/AppData.ts
@@ -1,4 +1,5 @@
 import { ICard, IAppState, IOrder } from "../types";
+import { CatalogItem } from "./Card";
 import { Model } from "./base/Model";
 
 export class ProductItem extends Model<ICard> {
@@ -26,5 +27,29 @@ export class AppState extends Model<IAppState> {
     this.catalog = items.map(item => new ProductItem(item, this.events));
     this.emitChanges('items:changed', { catalog: this.catalog });
   }
+
+  getPrice(container: CatalogItem[], value: string): string {
+		let totalAmount = 0;
+
+		for (let i = 0; i < container.length; i++) {
+			const current = container[i];
+			totalAmount += current.price;
+		}
+		return totalAmount + value;
+	}
+
+  addProduct(item: CatalogItem, container: CatalogItem[]) {
+		if (item) {
+			container.push(item);
+		}
+	}
+
+  clearBasket(container: CatalogItem[]) {
+		container.length = 0;
+	}
+
+  setOrder(state: IOrder) {
+		this.order = Object.assign(this.order, state);
+	}
 }
 
